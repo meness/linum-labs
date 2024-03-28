4c618bc1c055bb74767bf2005cbba78c84ba9779
Alex <aesshoferi@gmail.com>
2024-03-29 01:19:46 +0330

    fix: add key to mapped element

--- src/app/listing/_components/listing-card.component.tsx
+++ src/app/listing/_components/listing-card.component.tsx
@@ -22,9 +22,13 @@ export const ListingCard = () => {
   return (
     <div className="grid grid-flow-row gap-4 md:grid-cols-3">
       {isLoadingListing &&
-        new Array(6).fill(1).map(() => {
+        new Array(6).fill(1).map((_, index) => {
           return (
-            <Skeleton className="h-[300px] rounded-[var(--nextui-radius-large)] transition-transform hover:scale-[1.02] hover:opacity-100 active:scale-100 active:opacity-100" />
+            <Skeleton
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              className="h-[300px] rounded-[var(--nextui-radius-large)] transition-transform hover:scale-[1.02] hover:opacity-100 active:scale-100 active:opacity-100"
+            />
           );
         })}
       {!isLoadingListing &&
